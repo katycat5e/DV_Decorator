45e748c8b4daada77f099748e8d3b342a5240925
Katy Fox <katycat5e@gmail.com>
2021-01-30 15:59:58 -0500

    remove alias stuff for now

--- Redecorator.cs
+++ Redecorator.cs
@@ -86,68 +86,6 @@ namespace DVDecorator
 
                     TargetNames.Add(rootObjectName);
                 }
-
-                // check for aliases file
-                string aliasFile = Path.Combine(packDir.FullName, "aliases.txt");
-                if( File.Exists(aliasFile) )
-                {
-                    ProcessAliases(aliasFile, packDir.Name);
-                }
-            }
-        }
-
-        private static readonly Regex AliasRegex = new Regex(@"([^=]+)=([^=]+)", RegexOptions.Compiled);
-        public static void ProcessAliases( string aliasFilePath, string packName )
-        {
-            string[] aliases = File.ReadAllLines(aliasFilePath);
-            int lineNum = 1;
-
-            foreach( string line in aliases )
-            {
-                if( string.IsNullOrWhiteSpace(line) ) continue;
-
-                var match = AliasRegex.Match(line);
-                if( match.Success )
-                {
-                    string aliasedName = match.Groups[1].Value;
-                    string sourceName = match.Groups[2].Value;
-
-                    if( ObjectsToTexture.TryGetValue(sourceName, out ObjectGroup sourceGroup) )
-                    {
-                        TexturePackRoot sourceTextures = sourceGroup.TexturePacks.Find(tpg => tpg.PackName.Equals(packName));
-                        if( sourceTextures != null )
-                        {
-                            // found source
-                            if( !ObjectsToTexture.TryGetValue(aliasedName, out ObjectGroup aliasedGroup) )
-                            {
-                                // create new group if it doesn't exist yet
-                                aliasedGroup = new ObjectGroup(aliasedName);
-                                ObjectsToTexture.Add(aliasedName, aliasedGroup);
-                                TargetNames.Add(aliasedName);
-                            }
-
-                            // copy the texture references from the source to the aliased object
-                            var newGroup = new HeirarchicalTextureSet(aliasedName, sourceTextures.TextureSet);
-                            aliasedGroup.TexturePacks.Add(new TexturePackRoot(packName, newGroup));
-
-                            ModEntry.Logger.Log($"Object {aliasedName} will use same textures as {sourceName}");
-                        }
-                        else
-                        {
-                            ModEntry.Logger.Error($"Pack does not contain textures for source object {sourceName}, line {lineNum} of alias file {aliasFilePath}");
-                        }
-                    }
-                    else
-                    {
-                        ModEntry.Logger.Error($"Source object {sourceName} not found on line {lineNum} of alias file {aliasFilePath}");
-                    }
-                }
-                else
-                {
-                    ModEntry.Logger.Error($"Invalid line {lineNum} of alias file {aliasFilePath}");
-                }
-
-                lineNum++;
             }
         }
 
